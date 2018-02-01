dd60456a8e0382bec3180d5c1624f2eb75f7044b
Timm Friebe <thekid@thekid.de>
2018-02-01 23:43:27 +0100

    QA: Apidocs

--- src/main/php/web/rest/RestApi.class.php
+++ src/main/php/web/rest/RestApi.class.php
@@ -9,15 +9,23 @@ use lang\IllegalArgumentException;
 class RestApi implements Handler {
   private $delegates= [];
 
-  public function __construct($implementation) {
-    foreach (typeof($implementation)->getMethods() as $method) {
+  /** @param object $instance */
+  public function __construct($instance) {
+    foreach (typeof($instance)->getMethods() as $method) {
       foreach ($method->getAnnotations() as $verb => $path) { 
         $pattern= '#^'.$verb.':'.preg_replace('/\{([^}]+)\}/', '(?<$1>[^/]+)', $path).'$#';
-        $this->delegates[$pattern]= new Delegate($implementation, $method);
+        $this->delegates[$pattern]= new Delegate($instance, $method);
       }
     }
   }
 
+  /**
+   * Handle request
+   *
+   * @param  web.Request $req
+   * @param  web.Response $res
+   * @return var
+   */
   public function handle($req, $res) {
     $match= strtolower($req->method()).':'.$req->uri()->path();
     foreach ($this->delegates as $pattern => $delegate) { 
