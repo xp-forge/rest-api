8500dfde32707ae070dd8bf17f5132945d8bca39
Timm Friebe <thekid@thekid.de>
2018-02-06 15:29:07 +0100

    QA: APIdocs [skip ci]

--- src/main/php/web/rest/format/Json.class.php
+++ src/main/php/web/rest/format/Json.class.php
@@ -14,6 +14,13 @@ class Json extends EntityFormat {
   /** @return string */
   public function mimeType() { return 'application/json'; }
 
+  /**
+   * Reads entity from request
+   *
+   * @param  web.Request $request
+   * @param  string $name
+   * @return var
+   */
   public function read($request, $name) {
     $in= new StreamInput($request->stream());
     try {
@@ -23,6 +30,13 @@ class Json extends EntityFormat {
     }
   }
 
+  /**
+   * Writes entity to response
+   *
+   * @param  web.Response $response
+   * @param  string $name
+   * @return void
+   */
   public function write($response, $value) {
     $out= new StreamOutput($response->stream(), self::$FORMAT);
     try {
--- src/main/php/web/rest/format/OctetStream.class.php
+++ src/main/php/web/rest/format/OctetStream.class.php
@@ -7,10 +7,24 @@ class OctetStream extends EntityFormat {
   /** @return string */
   public function mimeType() { return 'application/octet-stream'; }
 
+  /**
+   * Reads entity from request
+   *
+   * @param  web.Request $request
+   * @param  string $name
+   * @return var
+   */
   public function read($req, $name) {
     throw new FormatException('Cannot deserialize binary data to an entity');
   }
 
+  /**
+   * Writes entity to response
+   *
+   * @param  web.Response $response
+   * @param  string $name
+   * @return void
+   */
   public function write($req, $value) {
     throw new FormatException('Cannot serialize entities to binary data');
   }
