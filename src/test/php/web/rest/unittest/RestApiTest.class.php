8bbd2c293c8fbbfbcd72c4465cee8e54bc7cf980
Timm Friebe <thekid@thekid.de>
2018-02-07 09:43:32 +0100

    QA: Refactor tests for clarity

--- src/test/php/web/rest/unittest/RestApiTest.class.php
+++ src/test/php/web/rest/unittest/RestApiTest.class.php
@@ -27,6 +27,25 @@ class RestApiTest extends TestCase {
     );
   }
 
+  /**
+   * Runs the handler
+   *
+   * @param  object $handler
+   * @param  string $method
+   * @param  string $uri
+   * @param  [:string] $headers
+   * @param  string $body
+   * @return web.Response
+   */
+  private function run($handler, $method, $uri, $headers= [], $body= null) {
+    $req= new Request(new TestInput($method, $uri, $headers, $body));
+    $res= new Response(new TestOutput());
+
+    (new RestApi($handler))->handle($req, $res);
+
+    return $res;
+  }
+
   #[@test]
   public function can_create() {
     new RestApi(new Users());
@@ -34,51 +53,31 @@ class RestApiTest extends TestCase {
 
   #[@test]
   public function list_users_returns_json() {
-    $req= new Request(new TestInput('GET', '/users'));
-    $res= new Response(new TestOutput());
-
-    (new RestApi(new Users()))->handle($req, $res);
-
+    $res= $this->run(new Users(), 'GET', '/users');
     $this->assertPayload(200, 'application/json', '{"1549":{"id":1549,"name":"Timm"},"6100":{"id":6100,"name":"Test"}}', $res);
   }
 
   #[@test]
   public function find_user_returns_json() {
-    $req= new Request(new TestInput('GET', '/users/1549'));
-    $res= new Response(new TestOutput());
-
-    (new RestApi(new Users()))->handle($req, $res);
-
+    $res= $this->run(new Users(), 'GET', '/users/1549');
     $this->assertPayload(200, 'application/json', '{"id":1549,"name":"Timm"}', $res);
   }
 
   #[@test]
   public function exception_raised_from_find_user_rendered_as_internal_server_error() {
-    $req= new Request(new TestInput('GET', '/users/0'));
-    $res= new Response(new TestOutput());
-
-    (new RestApi(new Users()))->handle($req, $res);
-
+    $res= $this->run(new Users(), 'GET', '/users/0');
     $this->assertPayload(500, 'application/json', '{"status":500,"message":"Undefined offset: 0"}', $res);
   }
 
   #[@test, @ignore('Not yet implemented')]
   public function type_errors_for_arguments_rendered_as_bad_request() {
-    $req= new Request(new TestInput('GET', '/users/not.an.int'));
-    $res= new Response(new TestOutput());
-
-    (new RestApi(new Users()))->handle($req, $res);
-
+    $res= $this->run(new Users(), 'GET', '/users/not.an.int');
     $this->assertPayload(400, 'application/json', '{"status":400,"message":"Expected integer for argument $id, have string"}', $res);
   }
 
   #[@test]
-  public function missing_arguments_rendered_as_bad_request() {
-    $req= new Request(new TestInput('POST', '/users'));
-    $res= new Response(new TestOutput());
-
-    (new RestApi(new Users()))->handle($req, $res);
-
+  public function missing_body_rendered_as_bad_request() {
+    $res= $this->run(new Users(), 'POST', '/users');
     $this->assertPayload(400, 'application/json', '{"status":400,"message":"Expecting a request body, none transmitted"}', $res);
   }
 
@@ -87,21 +86,13 @@ class RestApiTest extends TestCase {
     $body= '{"name":"New"}';
     $headers= ['Content-Type' => 'application/json', 'Content-Length' => strlen($body)];
 
-    $req= new Request(new TestInput('POST', '/users', $headers, $body));
-    $res= new Response(new TestOutput());
-
-    (new RestApi(new Users()))->handle($req, $res);
-
+    $res= $this->run(new Users(), 'POST', '/users', $headers, $body);
     $this->assertPayload(201, 'application/json', '{"id":6101,"name":"New"}', $res);
   }
 
   #[@test]
   public function get_user_avatar_streams_image() {
-    $req= new Request(new TestInput('GET', '/users/1549/avatar'));
-    $res= new Response(new TestOutput());
-
-    (new RestApi(new Users()))->handle($req, $res);
-
+    $res= $this->run(new Users(), 'GET', '/users/1549/avatar');
     $this->assertPayload(200, 'image/png', 'PNG...', $res);
   }
 
@@ -110,31 +101,19 @@ class RestApiTest extends TestCase {
     $body= 'PNG...';
     $headers= ['Content-Type' => 'image/png', 'Content-Length' => strlen($body)];
 
-    $req= new Request(new TestInput('PUT', '/users/1549/avatar', $headers, $body));
-    $res= new Response(new TestOutput());
-
-    (new RestApi(new Users()))->handle($req, $res);
-
+    $res= $this->run(new Users(), 'PUT', '/users/1549/avatar', $headers, $body);
     $this->assertEquals(204, $res->status());
   }
 
   #[@test]
   public function not_found() {
-    $req= new Request(new TestInput('GET', '/users/not.a.user/avatar'));
-    $res= new Response(new TestOutput());
-
-    (new RestApi(new Users()))->handle($req, $res);
-
+    $res= $this->run(new Users(), 'GET', '/users/not.a.user/avatar');
     $this->assertPayload(404, 'application/json', '{"status":404,"message":"No such user #not.a.user"}', $res);
   }
 
   #[@test]
   public function plain_output() {
-    $req= new Request(new TestInput('GET', '/monitoring/status'));
-    $res= new Response(new TestOutput());
-
-    (new RestApi(new Monitoring()))->handle($req, $res);
-
+    $res= $this->run(new Monitoring(), 'GET', '/monitoring/status');
     $this->assertEquals(
       "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 2\r\n\r\nOK",
       $res->output()->bytes()
@@ -143,11 +122,7 @@ class RestApiTest extends TestCase {
 
   #[@test]
   public function objects_are_marshalled() {
-    $req= new Request(new TestInput('GET', '/monitoring/details'));
-    $res= new Response(new TestOutput());
-
-    (new RestApi(new Monitoring()))->handle($req, $res);
-
+    $res= $this->run(new Monitoring(), 'GET', '/monitoring/details');
     $details= '{'.
       '"startup":"2018-06-02T14:12:11+0200",'.
       '"core":"XP9",'.
@@ -162,11 +137,7 @@ class RestApiTest extends TestCase {
     $body= '"2018-06-02T11:55:11+0200"';
     $headers= ['Content-Type' => 'application/json', 'Content-Length' => strlen($body)];
 
-    $req= new Request(new TestInput('PUT', '/monitoring/startup', $headers, $body));
-    $res= new Response(new TestOutput());
-
-    (new RestApi(new Monitoring()))->handle($req, $res);
-
+    $res= $this->run(new Monitoring(), 'PUT', '/monitoring/startup', $headers, $body);
     $this->assertPayload(200, 'application/json', $body, $res);
   }
 
@@ -175,11 +146,7 @@ class RestApiTest extends TestCase {
     $body= '{"id":1549,"name":"Timm"}';
     $headers= ['Content-Type' => 'application/json', 'Content-Length' => strlen($body)];
 
-    $req= new Request(new TestInput('PUT', '/monitoring/responsible', $headers, $body));
-    $res= new Response(new TestOutput());
-
-    (new RestApi(new Monitoring()))->handle($req, $res);
-
+    $res= $this->run(new Monitoring(), 'PUT', '/monitoring/responsible', $headers, $body);
     $this->assertPayload(200, 'application/json', $body, $res);
   }
 }
